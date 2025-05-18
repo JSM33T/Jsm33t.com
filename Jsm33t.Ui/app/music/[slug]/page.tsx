28bc099cabb0e7997eec00985bb9cc6829f62e58
jsm33t <jskainthofficial@gmail.com>
2025-05-18 23:50:25 +0530

    :rocket: linting be testing my patience

--- Jsm33t.Ui/app/music/[slug]/page.tsx
+++ Jsm33t.Ui/app/music/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import { getMusicBySlug } from '@/lib/music-data';
 import { notFound } from 'next/navigation';
 
-type Props = {
-	params: { slug: string };
-};
+// type Props = {
+// 	params: { slug: string };
+// };
 
 export default function MusicPage({ params }: { params: { slug: string } }) {
 	const music = getMusicBySlug(params.slug);
