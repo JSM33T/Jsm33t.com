59b061a85265efaa7c9016dae4c1eb1a1776a9d7
jsm33t <jasmeetsinghkainth@gmail.com>
2025-06-15 10:28:34 +0530

    flicker workaround

--- Jsm33t.Ui/components/helpers/PreloaderRemover.tsx
+++ Jsm33t.Ui/components/helpers/PreloaderRemover.tsx
@@ -7,8 +7,11 @@ export default function PreloaderRemover() {
 		const handleLoad = () => {
 			const el = document.querySelector('.page-loading');
 			if (!el) return;
-			el.classList.remove('active');
-			setTimeout(() => el.remove(), 4000);
+
+			setTimeout(() => {
+				el.classList.remove('active');
+				setTimeout(() => el.remove(), 500);
+			}, 2000);
 		};
 
 		if (document.readyState === 'complete') {
